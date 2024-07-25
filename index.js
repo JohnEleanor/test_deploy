a4fbe898c034b2c741883aae5494e1aeb1b1f176
johneleanor <mrs.ch64@gmail.com>
2024-07-25 10:55:41 +0700

    asdasd

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const Config = {
 
 const _client = new line.Client(Config);
 
+
 async function handleEvents(event) {
     console.log(event.type);
 }
@@ -48,55 +49,65 @@ app.get('/', (req, res) => {
 
 app.post('/v1/webhook', async (req, res) => {
     const events = req.body.events; 
-    events.forEach(event => {
-        const replyToken = event.replyToken;
-        const userMessage = event.message.text;
-        const userID = event.source.userId;
-      
-        if (event.type === 'message' && event.message.type === 'text') {
-            // ! เงื่อนไขสำหรับข้อความ
-            console.log('User ID :', userID);
-            console.log('User Message :', userMessage);
-            console.log('Replay Token :', replyToken);  
-            // เปลี่ยน Logic ตรงนี้
-            try {
+    if (!events) return res.status(400).send('Bad Request');
+    if (util.isArray(events)) {
+        events.forEach(event => {
+            const replyToken = event.replyToken;
+            const userMessage = event.message.text;
+            const userID = event.source.userId;
+        
+            if (event.type === 'message' && event.message.type === 'text') {
+                console.log("=================");
+                console.log('User ID :', userID);
+                console.log('User Message :', userMessage);
+                console.log('Replay Token :', replyToken);  
+                console.log("=================");
+                // เปลี่ยน Logic ตรงนี้
+                try {
+                    
+                    const resule = axios.get(`https://api.line.me/v2/bot/profile/${userID}`, { headers: headers })
+                    .then(response => {
+                        console.log('User Profile :', response.data);
+                        let replyText = '';
+                        if (greetings.includes(userMessage)) {
+                            replyText = `สวัสดีคะ คุณ ${response.data.displayName} มีอะไรให้ช่วยมั้ยค่ะ 🙏`;
+                        } else if (wellbeing.includes(userMessage)) {
+                            replyText = 'หนูสบายดีค่ะ ขอบคุณที่ถามค่ะ 🙏';
+                        } else if (thanks.includes(userMessage)) {
+                            replyText = 'ยินดีค่ะ มีอะไรให้ช่วยอีกมั้ยคะ 🙏';
+                        } else if (good.includes(userMessage)) {
+                            replyText = 'ดีค่ะ มีอะไรให้ช่วยมั้ยคะ 🙏';
+                        } else if (helpRequests.includes(userMessage)) {
+                            replyText = 'สวัสดีค่ะ มีอะไรให้ช่วยมั้ยคะ 🙏\nหากต้องการให้ดูเมนูอาหารเเละประเมิณเเคลสามารถส่งรูปภาพมาได้เลยค่ะ';
+                        } else if (addMenu.includes(userMessage)) {
+                            replyText = 'สวัสดีค่ะ สามารถส่งรูปภาพเมนูอาหารมาได้เลยค่ะ 🙏';
+                        } else {
+                            replyText = noUnderstand[Math.floor(Math.random() * noUnderstand.length)];
+                        }
+                        replyMessage(replyToken, replyText, userID);
+                        return res.status(200).send('OK');
+                    })
+                    .catch(error => {
+                        console.log('Error sending message:', error.message);
+                        return res.status(400).json({ message: 'Error sending message' , err : error});
+                    });
                 
-                axios.get(`https://api.line.me/v2/bot/profile/${userID}`, { headers: headers })
-                .then(response => {
-                    console.log('User Profile :', response.data);
-                    let replyText = '';
-                    if (greetings.includes(userMessage)) {
-                        replyText = `สวัสดีคะ คุณ ${response.data.displayName} มีอะไรให้ช่วยมั้ยค่ะ 🙏`;
-                    } else if (wellbeing.includes(userMessage)) {
-                        replyText = 'หนูสบายดีค่ะ ขอบคุณที่ถามค่ะ 🙏';
-                    } else if (thanks.includes(userMessage)) {
-                        replyText = 'ยินดีค่ะ มีอะไรให้ช่วยอีกมั้ยคะ 🙏';
-                    } else if (good.includes(userMessage)) {
-                        replyText = 'ดีค่ะ มีอะไรให้ช่วยมั้ยคะ 🙏';
-                    } else if (helpRequests.includes(userMessage)) {
-                        replyText = 'สวัสดีค่ะ มีอะไรให้ช่วยมั้ยคะ 🙏\nหากต้องการให้ดูเมนูอาหารเเละประเมิณเเคลสามารถส่งรูปภาพมาได้เลยค่ะ';
-                    } else if (addMenu.includes(userMessage)) {
-                        replyText = 'สวัสดีค่ะ สามารถส่งรูปภาพเมนูอาหารมาได้เลยค่ะ 🙏';
-                    } else {
-                        replyText = noUnderstand[Math.floor(Math.random() * noUnderstand.length)];
-                    }
-                    replyMessage(replyToken, replyText, userID);
-                })
-                .catch(error => {
-                    console.log('Error sending message:', error);
-                });
-               
-            } catch (error) {
-                console.log('Error sending message:', error.message);
-            }
-        } else if (event.type === 'message' && event.message.type === 'image') {
-            // ! เงื่อนไขสำหรับรูปภาพ
-            precessImage(replyToken, event.message.id, userID);
-            
+                } catch (error) {
+                    console.log('Error sending message:', error.message);
+                    return res.status(400).json({ message: 'Error sending message' , err : error});
+                }
+            } else if (event.type === 'message' && event.message.type === 'image') {
+                // ! เงื่อนไขสำหรับรูปภาพ
+                precessImage(replyToken, event.message.id, userID);
+                return res.status(200).send('OK');
 
-        }
-    });
-    res.sendStatus(200);
+            }
+        });
+    }else { 
+        console.log('No events found)');
+        return res.status(400).send('Bad Request');
+    }
+    // return res.sendStatus(400).send('Bad Request');
 });
 
 async function replyMessage(replyToken, message, userID) {
@@ -118,12 +129,16 @@ async function replyMessage(replyToken, message, userID) {
             })
             if (result.status === 200) {
                 console.log(result.data);
+                // return true;
             }else {
                 console.log('Error sending message:', result.data);
+                // return false;
             }
     } catch (error) {
         console.log('Error sending message:', error.message);
+        // return false;
     }
+
     
 }
 
