a7d3fccf8dbb8d4bd301ab12aabc571b4f9458cd
johneleanor <mrs.ch64@gmail.com>
2024-07-25 11:05:56 +0700

    fuck

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ app.get('/', (req, res) => {
 app.post('/v1/webhook', async (req, res) => {
     if (!events) return res.status(400).send('Bad Request');
     console.log('Webhook called');
-    console.log(req.body.events);
-    const events = req.body.events; 
-
+    
     if (util.isArray(events)) {
+        console.log(req.body.events);
+        const events = req.body.events; 
         events.forEach(event => {
             const replyToken = event.replyToken;
             const userMessage = event.message.text;
